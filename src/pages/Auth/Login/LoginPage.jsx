3fbd82a0f9577cddacb6c11897f39f2a763f9569
vijayyagik1 <lvyagik@gmail.com>
2023-04-20 15:31:57 +0530

    third

--- src/pages/Auth/Login/LoginPage.jsx
+++ src/pages/Auth/Login/LoginPage.jsx
@@ -1,6 +1,69 @@
+// import React, { useState } from 'react'
+// import styles from './LoginForm.module.css'
+// // import login1 from '../../../assets/Images';
+// import { useNavigate } from 'react-router-dom'
+// import Button from '../../../components/Buttons/Button'
+// import Navbar from '../../Navbar/Navbar'
+
+// const LoginPage = () => {
+//   const [username, setUsername] = useState('')
+//   const [password, setPassword] = useState('')
+
+//   const handleSubmit = e => {
+//     e.preventDefault()
+//     const userData = {
+//       username,
+//       password
+//     }
+//     localStorage.setItem('userData', JSON.stringify(userData))
+//   }
+//   const navigate = useNavigate()
+//   const handleClickRegister = () => {
+//     navigate('/register')
+//   }
+
+//   return (
+//     <div>
+//       <Navbar />
+//       <div className={styles.login}>
+//         <form onSubmit={handleSubmit} className={styles.form}>
+//           <label className={styles.label}>
+//             Email:
+//             <input
+//               type='text'
+//               value={username}
+//               onChange={e => setUsername(e.target.value)}
+//             />
+//           </label>
+//           <label>
+//             Password:
+//             <input
+//               type='password'
+//               value={password}
+//               onChange={e => setPassword(e.target.value)}
+//               className={styles.input}
+//             />
+//           </label>
+//           <button className={styles.button} type='submit'>
+//             Login{' '}
+//           </button>
+//           <Button
+//             onClick={handleClickRegister}
+//             buttonName='Register'
+//             className={styles.ress}
+//           />
+//         </form>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default LoginPage
+
+
 import React, { useState } from 'react'
+// import styles from './Login.module.css'
 import styles from './LoginForm.module.css'
-// import login1 from '../../../assets/Images';
 import { useNavigate } from 'react-router-dom'
 import Button from '../../../components/Buttons/Button'
 import Navbar from '../../Navbar/Navbar'
@@ -17,9 +80,25 @@ const LoginPage = () => {
     }
     localStorage.setItem('userData', JSON.stringify(userData))
   }
+
   const navigate = useNavigate()
   const handleClickRegister = () => {
     navigate('/register')
+  }
+
+  function handleLogin () {
+    const Users = JSON.parse(localStorage.getItem('user')) || []
+    const filteredUsers = Users.filter(user => {
+      return (
+        user.username.toString() === username.toString() &&
+        user.password.toString() === password.toString()
+      )
+    })
+    if (filteredUsers.length > 0) {
+      alert('Login success')
+    } else {
+      alert('Invalid credentials')
+    }
   }
 
   return (
@@ -44,8 +123,8 @@ const LoginPage = () => {
               className={styles.input}
             />
           </label>
-          <button className={styles.button} type='submit'>
-            Login{' '}
+          <button className={styles.button} type='submit' onClick={handleLogin}>
+            Login
           </button>
           <Button
             onClick={handleClickRegister}
@@ -58,4 +137,4 @@ const LoginPage = () => {
   )
 }
 
-export default LoginPage
+export default LoginPage;
